e65be28de0b1673223180ccaa967e3d1f75c61b2
rathiharshit123 <53534753+rathiharshit123@users.noreply.github.com>
2023-01-01 22:52:47 +0530

    signup functionality added

--- Server/src/user/user.module.ts
+++ Server/src/user/user.module.ts
@@ -3,8 +3,15 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import {user,userSchema} from './schemas/user.schema'
+import {JwtModule} from '@nestjs/jwt'
 @Module({
-  imports : [MongooseModule.forFeature([{name: user.name,schema: userSchema}])],
+  imports : [
+    MongooseModule.forFeature([{name: user.name,schema: userSchema}]),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: {expiresIn: '1d'}
+    })
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
--- Server/src/user/user.service.ts
+++ Server/src/user/user.service.ts
@@ -1,16 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, Injectable, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { signUpDto } from './dto/signup.dto';
 import { user, userDocument } from './schemas/user.schema';
-
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class UserService {
 
-  constructor(@InjectModel(user.name) private userModel: Model<userDocument>) {}
+  constructor(
+    @InjectModel(user.name) private userModel: Model<userDocument>,
+    private readonly JwtService: JwtService  
+  ) {}
   async signUp(request: signUpDto) {
-    const newUser = new this.userModel(request);
-    let response = await newUser.save();
-    return response;
+    try {
+      let checkUserExist = await this.userModel.findOne({email: request.email});
+      if(checkUserExist){
+        return new HttpException('email alredy exists',HttpStatus.BAD_REQUEST)
+      }
+
+      const hashPassword = await bcrypt.hash(request.password,10);
+      request.password = hashPassword;
+
+      const newUser = new this.userModel(request);
+      let userData = await newUser.save();
+
+      let tokenData = {
+        id: userData.id,
+        name: userData.name,
+      }
+
+      const token = await this.JwtService.signAsync(tokenData);
+
+      let responseObject = {
+        code: 200,
+        message: "User Saved Successfully",
+        data : {
+          token,
+          ...tokenData
+        },
+      }
+      return responseObject;
+    } catch (error) {
+      console.log({
+        error,
+        msg: "error while signup"
+      })
+      throw error;
+    }
   }
 }
